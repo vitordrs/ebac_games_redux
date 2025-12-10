f0354c5bad491d48ca48ea1fdbdb8b36b908b0c4
vitordrs <soaresvitor196@gmail.com>
2025-12-10 14:47:17 -0300

    commit inicial da aula

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import { useSelector } from 'react-redux'
+
+import * as S from './styles'
+
+import cesta from '../../assets/cesta.png'
+import { paraReal } from '../Produto'
+
+import { RootReducer } from '../../store'
+
+const Header = () => {
+  const itens = useSelector((state: RootReducer) => state.carrinho.itens)
+
+  const valorTotal = itens.reduce((acc, item) => {
+    acc += item.preco
+    return acc
+  }, 0)
+
+  return (
+    <S.Header>
+      <h1>EBAC Games</h1>
+      <div>
+        <img src={cesta} />
+        <span>
+          {itens.length} itens, valor total: {paraReal(valorTotal)}
+        </span>
+      </div>
+    </S.Header>
+  )
+}
+
+export default Header
